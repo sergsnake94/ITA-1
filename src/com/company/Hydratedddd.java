eb74d6beba2bc4decc3158945c7ffc98dd794d43
US18095196754\Admin555 <snakedrummm@gmail.com>
2021-11-24 15:36:26 +0200

    First

---
+++ src/com/company/Hydratedddd.java
@@ -0,0 +1,9 @@
+package com.company;
+public class Hydratedddd {
+        public int Liters(double time) {
+
+            double result = time / 2;
+            int convertedtoint = (int) Math.floor(result);
+            return convertedtoint;
+        }
+    }
---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Hydratedddd hydratedddd = new Hydratedddd();
+        System.out.println( hydratedddd.Liters(41));
+    }
+}
